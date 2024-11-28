afd68ca2c1eb9c2eb4e4424ef6bf9587cfb0344c
Denis Khairutdinov <jackrassul@icloud.com>
2024-11-28 20:56:01 +0500

    delete event

---
+++ src/03_article-cover/video.js
@@ -0,0 +1,12 @@
+const options = {
+    id: 725848418,
+    autoplay: true,
+    muted: true,
+    loop: true,
+    responsive: true,
+    unmute_button: false,
+    keyboard: false,
+    controls: false,
+};
+
+const player = new Vimeo.Player('article-cover__video', options);
